5f67fae208f246da01bd8d8c3d6a3e4b16a4d779
Enej <enej.licina@windowslive.com>
2020-04-19 15:31:46 +0200

    app revamp

---
+++ QuizApp-iOS13/Controller/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  QuizApp-iOS13
+//
+//  Created by Enej on 19/04/2020.
+//  Copyright © 2020 Enej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+            sender.setTitleColor(UIColor.black, for: .normal)
+        } else {
+            sender.backgroundColor = UIColor.red
+            sender.setTitleColor(UIColor.black, for: .normal)
+        }
+        
+        quizBrain.nextQuestion()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.updateUI()
+        }
+        //updateUI()
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+        trueButton.setTitleColor(UIColor.white, for: .normal)
+        falseButton.setTitleColor(UIColor.white, for: .normal)
+    }
+
+}
+
